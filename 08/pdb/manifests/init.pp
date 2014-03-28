eaa0c06452af02b5c8eeb163deb6f31f2426c97c
Sample Developer <samdev@example.com>
2014-03-28 18:07:51 -0400

    start of chapter 8 files

---
+++ 08/pdb/manifests/init.pp
@@ -0,0 +1,16 @@
+class pdb {
+  # puppetdb class
+  class { 'puppetdb::server': }
+  class { 'puppetdb::database::postgresql': listen_addresses => '*' }
+  #firewall {'5432 postgresql':
+  #  action => 'accept',
+  #  proto  => 'tcp',
+  #  dport  => '5432',
+  #}
+  #firewall {'8081 puppetdb':
+  #  action => 'accept',
+  #  proto  => 'tcp',
+  #  dport  => '8081',
+  #}
+}
+
