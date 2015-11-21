7e18ebaaae20dc579ae40706b7aacd554727b39f
Sample Developer <samdev@example.com>
2015-11-21 15:46:51 -0500

    more lint and 4 changes

--- 06/modules/example/manifests/fs.pp
+++ 06/modules/example/manifests/fs.pp
@@ -1,55 +1,56 @@
 define example::fs
 (
-  $mnt     = "$title",   # where to mount the filesystem
-  $vg      = 'VolGroup', # which volume group
-  $pv,                   # which physical volume
-  $lv,                   # which logical volume
-  $fs_type = 'ext4',     # the filesystem type
-  $size,                 # how big
-  $managed = true,       # do we create the file resource or not.
-  $owner   = '0',        # who owns the mount point
-  $group   = '0',        # which group owns the mount point
-  $mode    = '0755'      # permissions on mount point
+  String $lv,                   # which logical volume
+  String $pv,                   # which physical volume
+  Number $size,                 # how big
+
+  Enum['ext4','ext3','xfs'] $fs_type = 'ext4',     # the filesystem type
+  String $mnt     = $title,   # where to mount the filesystem
+  String $vg      = 'VolGroup', # which volume group
+  Bool $managed   = true,       # do we create the file resource or not.
+  String $owner   = '0',        # who owns the mount point
+  String $group   = '0',        # which group owns the mount point
+  String $mode    = '0755'      # permissions on mount point
 ) {
   # create the filesystem
-  lvm::volume {"$lv":
+  lvm::volume {$lv:
     ensure => 'present',
-    vg     => "$vg",
-    pv     => "$pv",
-    fstype => "$fs_type",
-    size   => "$size",
+    vg     => $vg,
+    pv     => $pv,
+    fstype => $fs_type,
+    size   => $size,
   }
 
   # create the mount point (mnt)
   if ($managed) {
-    file {"$mnt":
+    file {$mnt:
       ensure => 'directory',
-      owner  => "$owner",
-      group  => "$group",
-      mode   => "$mode",
+      owner  => $owner,
+      group  => $group,
+      mode   => $mode,
     }
-    mount {"$lv":
-      name    => "$mnt",
+    mount {$lv:
       ensure  => 'mounted',
-      device  => "/dev/$vg/$lv",
+      name    => $mnt,
+      device  => "/dev/${vg}/${lv}",
       dump    => '1',
-      fstype  => "$fs_type",
-      options => "defaults",
+      fstype  => $fs_type,
+      options => 'defaults',
       pass    => '2',
       target  => '/etc/fstab',
-      require => [Lvm::Volume["$lv"],File["$mnt"]],
+      require => [Lvm::Volume[$lv],File[$mnt]],
     }
   } else {
-    mount {"$lv":
-      name    => "$mnt",
+    mount {$lv:
       ensure  => 'mounted',
-      device  => "/dev/$vg/$lv",
+      name    => $mnt,
+      device  => "/dev/${vg}/${lv}",
       dump    => '1',
-      fstype  => "$fs_type",
-      options => "defaults",
+      fstype  => $fs_type,
+      options => 'defaults',
       pass    => '2',
       target  => '/etc/fstab',
-      require => Lvm::Volume["$lv"],
+      require => Lvm::Volume[$lv],
     }
   }
 }
