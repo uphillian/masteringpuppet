bf79e0f3f06aaafa65a7ac1c8c58250546cfb2df
Sample Developer <samdev@example.com>
2015-11-21 16:49:45 -0500

    lint

--- 06/modules/example/manifests/admin.pp
+++ 06/modules/example/manifests/admin.pp
@@ -1,36 +1,34 @@
 define example::admin
 (
-  $user = $title,
-  $ensure = 'present',
-  $uid,
-  $home = "/var/home/$title",
-  $mode = '0750',
-  $shell = "/bin/bash",
-  $bashrc = undef,
-  $bash_profile = undef,
-  $groups = ['wheel','bin'],
-  $comment = "$title Admin User",
-  $expiry = 'absent',
-  $forcelocal = true,
+  Integer $uid,
   $key,
+  String $user = $title,
+  Enum['present','absent'] $ensure = 'present',
+  String $home = "/var/home/${title}",
+  String $mode = '0750',
+  Enum['/bin/bash','/bin/ksh'] $shell = '/bin/bash',
+  String $bashrc = '',
+  String $bash_profile = '',
+  Array[String] $groups = ['wheel','bin'],
+  String $comment = "${title} Admin User",
+  String $expiry = 'absent',
+  $forcelocal = true,
   $keytype = 'ssh-rsa',
-)
-{
+) {
   include example::admin::group
-  user { "$user":
+  user { $user:
     ensure     => $ensure,
-    allowdupe  => 'true',
-    comment    => "$comment",
-    expiry     => "$expiry",
+    allowdupe  => true,
+    comment    => $comment,
+    expiry     => $expiry,
     forcelocal => $forcelocal,
     groups     => $groups,
-    home       => "$home",
-    shell      => "$shell",
+    home       => $home,
+    shell      => $shell,
     uid        => $uid,
     gid        => 'admin',
     require    => Group['admin']
   }
-  
   # ensure the home directory location exists
   $grouprequire = Group['admin']
   $dirhome = dirname($home)
@@ -49,61 +47,61 @@ define example::admin
       $homerequire = [$grouprequire,File[$dirhome]]
     }
   }
-  file {"$home":
+  file {$home:
     ensure  => 'directory',
-    owner   => "$uid",
+    owner   => $uid,
     group   => 'admin',
-    mode    => "$mode",
+    mode    => $mode,
     require => $homerequire
   }
 
   # ensure the .ssh directory exists
-  file {"$home/.ssh":
+  file {"${home}/.ssh":
     ensure  => 'directory',
-    owner   => "$uid",
+    owner   => $uid,
     group   => 'admin',
-    mode    => "0700",
-    require => File["$home"]
-  } 
-  ssh_authorized_key { "$user-admin":
-    user   => "$user",
-    ensure => present,
-    type   => "$keytype",
-    key    => "$key",
-    require => [User[$user],File["$home/.ssh"]]
+    mode    => '0700',
+    require => File[$home]
+  }
+  ssh_authorized_key { "${user}-admin":
+    ensure  => present,
+    user    => $user,
+    type    => $keytype,
+    key     => $key,
+    require => [User[$user],File["${home}/.ssh"]]
   }
 
   # build up the bashrc from a concat
-  concat { "$home/.bashrc":
+  concat { "${home}/.bashrc":
     owner => $uid,
-    group => $gid,
-  } 
-  concat::fragment { "bashrc_header_$user":
-    target => "$home/.bashrc",
+    group => 1001,
+  }
+  concat::fragment { "bashrc_header_${user}":
+    target => "${home}/.bashrc",
     source => '/etc/skel/.bashrc',
     order  => '01',
   }
-  if $bashrc != undef {
-    concat::fragment { "bashrc_user_$user":
-      target  => "$home/.bashrc",
+  if $bashrc != '' {
+    concat::fragment { "bashrc_user_${user}":
+      target  => "${home}/.bashrc",
       content => $bashrc,
       order   => '10',
     }
   }
 
   #build up the bash_profile from a concat as well
-  concat { "$home/.bash_profile":
+  concat { "${home}/.bash_profile":
     owner => $uid,
-    group => $gid,
+    group => 1001,
   }
-  concat::fragment { "bash_profile_header_$user":
-    target => "$home/.bash_profile",
+  concat::fragment { "bash_profile_header_${user}":
+    target => "${home}/.bash_profile",
     source => '/etc/skel/.bash_profile',
     order  => '01',
   }
-  if $bash_profile != undef {
-    concat::fragment { "bash_profile_user_$user":
-      target  => "$home/.bash_profile",
+  if $bash_profile != '' {
+    concat::fragment { "bash_profile_user_${user}":
+      target  => "${home}/.bash_profile",
       content => $bash_profile,
       order   => '10',
     }
