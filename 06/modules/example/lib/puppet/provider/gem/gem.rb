66b89d58b3496ea265278e923402873350e7e2cf
Thomas Uphill <thomas.uphill@wellsfargo.com>
2015-11-23 22:11:38 -0800

    alignment

--- 06/modules/example/lib/puppet/provider/gem/gem.rb
+++ 06/modules/example/lib/puppet/provider/gem/gem.rb
@@ -1,19 +1,19 @@
 Puppet::Type.type(:gem).provide(:gem) do
   commands :gem => "gem"
   def exists?
-          begin
-                  gem= resource[:version] ? "%{resource[:name]} --version #{resource[:version]}" | resource[:name]
-                  gem('list', '-i', gem)
-          rescue Puppet::ExecutionFailure => e
-                  false
-          end
+    begin
+      gem= resource[:version] ? "%{resource[:name]} --version #{resource[:version]}" | resource[:name]
+      gem('list', '-i', gem)
+    rescue Puppet::ExecutionFailure => e
+      false
+    end
   end
   def create
-          gem= resource[:version] ? "%{resource[:name]} --version #{resource[:version]}" | resource[:name]
-          gem('install', gem)
+    gem= resource[:version] ? "%{resource[:name]} --version #{resource[:version]}" | resource[:name]
+    gem('install', gem)
   end
   def destroy
-          gem= resource[:version] ? "%{resource[:name]} --version #{resource[:version]}" | resource[:name]
-          gem('uninstall', gem, '-q', '-x')
+    gem= resource[:version] ? "%{resource[:name]} --version #{resource[:version]}" | resource[:name]
+    gem('uninstall', gem, '-q', '-x')
   end
 end
