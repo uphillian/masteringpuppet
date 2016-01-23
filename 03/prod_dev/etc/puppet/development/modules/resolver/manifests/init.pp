619dd4c512716270954678f8bc5e5b6f4bc47890
Thomas Uphill <thomas@narrabilis.com>
2016-01-22 22:04:20 -0800

    remove old

--- 03/prod_dev/etc/puppet/development/modules/resolver/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class resolver($search = "example.com") {
-  augeas { 'set resolv.conf search':
-    context => '/files/etc/resolv.conf',
-    changes => [
-      "set search/domain '${search}'"
-    ],
-  }
-}
--- 03/prod_dev/etc/puppet/production/modules/hostname_problem/manifests/init.pp
+++
@@ -1,3 +0,0 @@
-class hostname_problem ($enc_hostname) {
-  notify {"WARNING: $enc_hostname ($::ipaddress) doesn't conform to naming standards": }
-}
